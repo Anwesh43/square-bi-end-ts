c88e87b489c0ad6ea94efd683f2330a72fbf1374
Anwesh43 <anweshthecool0@gmail.com>
2021-04-05 02:31:45 +0530

    adding sbenODE CLASS

--- index.ts
+++ index.ts
@@ -142,3 +142,45 @@ class Animator {
         } 
     }
 }
+
+class SBENode {
+
+    state : State = new State()
+    next : SBENode 
+    prev : SBENode 
+
+    constructor(private i : number) {
+        
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new SBENode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawSBENode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : SBENode {
+        var curr : SBENode = this.prev 
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
